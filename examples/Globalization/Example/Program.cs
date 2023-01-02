14cf980579999856a2c0ab7ca9f8984c4af1d173
Mikhail Korolev <michael443959@gmail.com>
2023-01-02 16:38:15 +0300

    Add example

---
+++ examples/Globalization/Example/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections;
+using System.Globalization;
+
+IEnumerable<RegionInfo> AllRegionInfo = 
+CultureInfo.GetCultures(CultureTypes.SpecificCultures)
+    .Where(culture => culture.LCID != 0x7F) // filer invariant culture
+    .Select(culture => new RegionInfo(culture.Name))
+    .ToList();
+
+Console.WriteLine(string.Join(Environment.NewLine,AllRegionInfo));
